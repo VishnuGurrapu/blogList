bf9efd039c00b111b10197407d8f7933c69144aa
VishnuGurrapu <gurrapuvishnu20@gmail.com>
2024-08-21 18:47:33 +0000

    submission commit

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,17 @@
+// Write your JS code here
+import './index.css'
+const Userinfo = () => {
+  return (
+    <div className="userinfoContainer">
+      <img
+        alt="profile"
+        className="profile"
+        src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      />
+      <h1 className="name">Wade Warren</h1>
+      <p className="des">Software developer at UK</p>
+    </div>
+  )
+}
+
+export default Userinfo
